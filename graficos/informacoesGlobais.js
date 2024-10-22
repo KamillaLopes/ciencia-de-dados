fbf08495e783357028ba4d3051e5ea215406ba60
Kamilla Lopes <105080698+KamillaLopes@users.noreply.github.com>
2024-10-22 07:56:00 -0300

    ok

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -5,7 +5,7 @@ async function visualizarInformacoesGlobais() {
         const dados = await res.json()
         const pessoaConectadas = (dados.total_pessoas_conectadas) / 1e9
         const pessoasNoMundo = (dados.total_pessoas_mundo) / 1e9
-        const horas = parseInt = (dados.tempo_medio)
+        const horas = parseInt (dados.tempo_medio)
         const paragrafo = document.createElement('p')
         paragrafo.classList.add('graficos-container__texto')
         paragrafo.innerHTML = `Você sabia que o mundo tem <span>${pessoasNoMundo} bilhões </span>
