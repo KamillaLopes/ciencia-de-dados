f9867bf5ef498eb4811a9e2279fb3e99a253e0d4
Kamilla Lopes <105080698+KamillaLopes@users.noreply.github.com>
2024-10-23 11:51:36 -0300

    ok

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -11,7 +11,8 @@ async function visualizarInformacoesGlobais() {
         paragrafo.classList.add('graficos-container__texto')
         paragrafo.innerHTML = `Você sabia que o mundo tem <span>${pessoasNoMundo} bilhões </span>
         de pessoas e que aproximadamente <span>${pessoaConectadas} bilhões </span> estão 
-        conectadas em alguma rede social e passam em média <span>${horas} horas </span> e <span> ${minutos} minutos </span> conectadas.`
+        conectadas em alguma rede social e passam em média <span>${horas} horas </span> e <span> ${minutos} minutos </span> conectadas.
+        <br> Isso significa que aproximadamente ${porcentagemConectada}% de pessoas estão conectadas em alguma rede social.`
 
         const container = document.getElementById ('graficos-container')
         container.appendChild(paragrafo)
