6249252e490206ec6447023c562d8afa7259b297
Kamilla Lopes <105080698+KamillaLopes@users.noreply.github.com>
2024-11-26 07:51:42 -0300

    ok

--- graficos/quantidadeUsuarios.js
+++ graficos/quantidadeUsuarios.js
@@ -53,7 +53,7 @@ async function quantidadeUsuarios() {
     const grafico = document.createElement('div')
     grafico.className = 'grafico'
     document.getElementById('graficos-container').appendChild(grafico)
-    Plotly.newPlot(grafico, data)
+    Plotly.newPlot(grafico, layout, data)
     
     }
     
