99e55db824f99e47be62fb8f2ee65e5917be81ce
Kamilla Lopes <105080698+KamillaLopes@users.noreply.github.com>
2024-12-03 07:43:18 -0300

    okl

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -1,4 +1,4 @@
-const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
+const url = 'https://raw.githubusercontent.com/KamillaLopes/ciencia-de-dados/refs/heads/main/basededados/esportes-dados-globais.json'
 
 async function visualizarInformacoesGlobais() {
         const res = await fetch(url)
